bb8fa7613e1bf8040aa5d427dee9bbd4bc9b3c8e
6l3m <clmpoulain@gmail.com>
2019-10-06 22:08:02 +0200

    slapi url

--- src/api/controllers/ads-controller.js
+++ src/api/controllers/ads-controller.js
@@ -4,17 +4,19 @@ export default (router) => {
 
   router.post('/slapi/ads',
     async (req, res) => {
+      const _inseeCodeArr = req.body.inseeCode.split('');
+      _inseeCodeArr.splice(2, 0, '0');
       const adsDTO = {
-        zipCode: req.body.zipCode,
+        inseeCode: _inseeCodeArr.join(''),
         budget: req.body.budget 
       };
       try {
         const url = `https://www.seloger.com/list.htm?tri=initial&enterprise=0&idtypebien=2,1`
-          + `&pxMax=${adsDTO.budget}&idtt=1&naturebien=1&ci=${adsDTO.zipCode}`;
+          + `&pxMax=${adsDTO.budget}&idtt=1&naturebien=1&ci=${adsDTO.inseeCode}`;
         const resp = await Axios.get(url);
         res.json({ status: 'OK', resp: JSON.stringify(resp.data)
           .match('var ava_data =(.*)ava_data.logged')[1]
-          .replace(/\\r?\\n|\\r|\\|;/g, '')
+          .replace(/\\r?\\n|\\r|\\|;/g, '') 
         });
       } catch (error) {
         res.status(500).json({ status: 'KO' });
